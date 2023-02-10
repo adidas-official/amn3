e74ccc2ef366144673e66ccafab5a4dbfbe9796a
zf <zdenek.frydryn@gmail.com>
2023-02-10 22:22:21 +0100

    Added class XScout
    XScout class maps xslx file and find first and last line of data.
    Makes list of ids for each listed sheet. This will be later used to
    compare data from xlsx and csv.

--- vanguard.py
+++ vanguard.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from servant import clean, get_month, get_pension_type, get_ins_code
+import openpyxl
+import openpyxl.utils
 
 
 class Vanguard:
@@ -54,4 +56,53 @@ class Vanguard:
 
 
 d = Vanguard(file_mzdy='data/Q2.CSV', file_pracov='data/PRACOVQ2.CSV')
-print(d.merge_data)
+
+
+class XScout:
+    def __init__(self, spreadsheet):
+        self.wb = openpyxl.load_workbook(spreadsheet)
+        self.spread = self.spread()
+
+    def employee_list(self):
+        people = []
+        for sheet_index, ws in enumerate(self.wb.worksheets[1:3]):
+            sheet_ids = []
+            for row in range(*self.spread[sheet_index]):
+                sheet_ids.append(ws.cell(row, 4).value)
+            people.append(sheet_ids)
+
+        return people
+
+    def first_row(self, sheet_number):
+        ws = self.wb.worksheets[sheet_number]
+        row = 1
+        while True:
+            if ws.cell(row, 1).value == 1:
+                return row
+            row += 1
+
+    def last_row(self, sheet_number):
+        ws = self.wb.worksheets[sheet_number]
+        row = self.first_row(sheet_number)
+
+        while True:
+            if not ws.cell(row, 2).value:
+                return row - 1
+            row += 1
+
+    def spread(self):
+        s = []
+        for sheet_number in range(1, 3):
+            spread = (self.first_row(sheet_number), self.last_row(sheet_number))
+            s.append(spread)
+        return s
+
+
+x = XScout('tables/jmenny_seznam_2022_09_27 Bereko.xlsx')
+x.employee_list()
+for sheet in x.employee_list():
+    print('Sheet:')
+    for i in sheet:
+        print(i)
+
+
